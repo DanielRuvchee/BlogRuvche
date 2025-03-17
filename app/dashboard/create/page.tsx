3c2e3a00cf3c716bd9e9544d810dbdbd89012f5d
Daniel <daniel.ruvcheski@students.finki.ukim.mk>
2025-03-17 22:48:40 +0100

    Update Dashboard page layout with a link to create posts, and enhance CSS variables for better styling control.

---
+++ app/dashboard/create/page.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Card,  CardContent,  CardDescription,  CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Create() {
+    return (
+        <div>
+            <Card className="max-w-lg m-auto">
+                <CardHeader>
+                    <CardTitle>Create Post</CardTitle>
+                    <CardDescription>Create a new post to share it.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                
+                    <form className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-2">
+                            <Label>Title</Label>
+                            <Input required type="text" placeholder="Title" />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <Label>Content</Label>
+                            <Textarea required placeholder="Content" />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <Label>Image URL</Label>
+                            <Input required type="url" placeholder="Image URL" />
+                        </div>
+
+                        <Button>Create Post</Button>
+                    </form>
+                    
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
