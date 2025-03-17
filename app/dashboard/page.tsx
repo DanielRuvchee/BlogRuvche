a54fa436040949d93d77cb404cf596fa7e606305
Daniel <daniel.ruvcheski@students.finki.ukim.mk>
2025-03-17 21:31:18 +0100

    Implement user redirection in Dashboard component for unauthenticated access, enhancing Kinde Auth integration.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { redirect } from "next/navigation"
 
 export default async function Dashboard() {
-    const { getUser } = getKindeServerSession
+    const { getUser } = getKindeServerSession()
     const user = await getUser()
 
     if(!user) {
-        
+        return redirect("/api/auth/register")
     }
     return (
         <div>
