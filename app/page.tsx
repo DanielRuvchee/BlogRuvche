05f9d28601d00d541121b1b5ba51a83552353dcb
Daniel <daniel.ruvcheski@students.finki.ukim.mk>
2025-03-18 20:23:29 +0100

    Refactor Dashboard and Page Components: Update routing and manifest files, enhance CSS for animations, and improve data fetching in the Home component with a loading delay. Integrate BlogPostCards for dynamic post rendering.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
 import Link from "next/link"
 import { prisma } from "./utils/db";
+import BlogPostCards from "@/components/general/BlogPostCards";
 
 async function getData() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))//Fake Delay for showing loading
 
   const data = await prisma.blogPost.findMany({
     select: {
@@ -21,17 +23,25 @@ async function getData() {
   return data;
 }
 
-export default async function Home() {
-  const data = await getData()
+export default  function Home() {
+  
+
   return ( 
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
 
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data.map((item) => (
-          <h1 key={item.title}>{ item.title }</h1>
-        ))}
-      </div>
+    <BlogPost />
     </div>
   );
+}
+
+async function BlogPost() {
+  const data = await getData()
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <BlogPostCards key={item.id} data={item} />
+        ))}
+      </div>
+  )
 }
